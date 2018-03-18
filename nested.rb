7691666455b0fc87db87c954e00c1b051e2c0551
Emily Welsch <emwelsch@gmail.com>
2018-03-18 21:39:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-require 'pry';
+require 'pry'; binding.pry
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -88,7 +88,7 @@ programmer_hash[:yukihiro_matsumoto] = {
   :languages => ["LISP", "C"]
 }
 programmer_hash
-binding.pry
+
 end
 
 def changing_alan
