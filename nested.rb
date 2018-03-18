45960e79e8cdfe17c78ad8ce3690e43bd9be05fa
Emily Welsch <emwelsch@gmail.com>
2018-03-18 21:48:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-# require 'pry'; binding.pry
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -88,11 +88,11 @@ programmer_hash[:yukihiro_matsumoto] = {
   :languages => ["LISP", "C"]
 }
 programmer_hash
-
 end
 
 def changing_alan
-	programmer_hash =
+# require 'pry'; binding.pry
+  programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -110,7 +110,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-
+     programmer_hash[:alan_kay] = {
+       :known_for => "GUI"
+     }
 end
 
 def adding_to_dennis
